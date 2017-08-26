21e02718c69b65f8a02ceb62947231455d17a1d3
CreepaCat <2469330518@qq.com>
2017-08-26 16:45:39 +0800

    push

--- src/HowIWorld.java
+++ src/HowIWorld.java
@@ -3,6 +3,6 @@ import static java.lang.System.out;
 public class HowIWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world");
-        System.out.println("Hi");
+        System.out.println("push");
     }
 }
